b07777d50937a3e97f40d0c5761a1fbfcc46c784
Zhiming <i-jovial@hotmail.com>
2022-03-08 22:21:45 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# GKE Cluster with Terraform
+
+This sample repo creates a VPC and subnet for the GKE cluster. This is not
+required but highly recommended to keep your GKE cluster isolated.
+
+The GKE Cluster targets a zone which results in 2 nodes created as a sample terraform workflow.
+
+
+## References
+
+- [Provision a GKE Cluster learn guide](https://learn.hashicorp.com/terraform/kubernetes/provision-gke-cluster)
---
+++ variables.tf
@@ -0,0 +1,28 @@
+variable "project_id" {
+  description = "project id"
+}
+
+variable "region" {
+  description = "region"
+}
+
+variable "zone" {
+  description = "zone"
+}
+
+variable "credentials_file" { }
+
+variable "gke_username" {
+  default     = ""
+  description = "gke username"
+}
+
+variable "gke_password" {
+  default     = ""
+  description = "gke password"
+}
+
+variable "gke_num_nodes" {
+  default     = 3
+  description = "number of gke nodes"
+}
